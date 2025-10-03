d84d709c592914a8081d61cb14df800d9555e2e3
HasaniPunchihewa <hasanipunchihewa@gmail.com>
2025-10-03 11:57:36 +0530

    Adding comments to main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
+# This provider block sets up AWS in the specified region
 provider "aws" {
   region = var.aws_region
 }
 
+# This resource creates an S3 bucket
 resource "aws_s3_bucket" "sample_bucket" {
   bucket = var.bucket_name
   acl    = "private"
